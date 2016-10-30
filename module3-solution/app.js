85cb55b40b5d3029060f2480bbb1527b7d9fb641
Polina <Polina>
2016-10-30 11:06:45 +0200

    module3 solution

--- module3-solution/app.js
+++ module3-solution/app.js
@@ -4,7 +4,32 @@
   angular.module('NarrowItDownApp', []).
   controller('NarrowItDownController', NarrowItDownController).
   service('MenuSearchService', MenuSearchService).
+  directive('foundItems', FoundItemsDirective).
   constant('ServerUrl', 'https://davids-restaurant.herokuapp.com/menu_items.json');
+
+  function FoundItemsDirective() {
+    var ddo= {
+      restrict: 'E',
+      templateUrl: 'foundItems.html',
+      transclude: true,
+      scope: {
+        foundItems: '<',
+        onRemove: '&'
+      },
+      controller: FoundDirectiveController,
+      controllerAs: 'menu',
+      bindToController: true
+    };
+    return ddo;
+  };
+
+  function FoundDirectiveController() {
+    var menu = this;
+
+    // menu.noItemsFound = function() {
+    //   return menu.foundItems.length ===0;
+    // };
+  };
 
   MenuSearchService.$inject = ['$http', 'ServerUrl']
   function MenuSearchService($http, ServerUrl) {
@@ -17,8 +42,10 @@
       }).then(function(result) {
         //process items and only keep the item that match the provided input
         var foundItems = result.data;
-        console.log('found items', foundItems);
-        return foundItems;
+        return foundItems.menu_items.filter(function (i) {
+          return searchTerm.length && i.description.indexOf(searchTerm) !== -1;
+        });
+        // return foundItems;
       });
     };
   };
@@ -27,14 +54,26 @@
   function NarrowItDownController(MenuSearchService) {
     var menu = this;
 
-    menu.narrowDownMenu = function(searchTerm) {
-      var foundItems =  MenuSearchService.getMatchedMenuItems(searchTerm);
+    menu.searchTerm = '';
+
+    menu.narrowDownMenu = function() {
+      menu.foundItems = [];
+      var foundItems =  MenuSearchService.getMatchedMenuItems(menu.searchTerm);
       foundItems.then(function(result) {
-          console.log('ctrl found items', result);
+          menu.foundItems = result;
+          console.log(menu.foundItems);
+          if(menu.foundItems.length === 0) {
+            menu.displayMsg = true;
+          }else {
+            menu.displayMsg = false;
+          }
       });
     };
+
+    menu.removeItem = function(index) {
+      menu.foundItems.splice(index, 1);
+    };
+
   };
-
-
 
 })();
