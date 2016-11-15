fabc97b71b52904e0be2d3954203f8872d63b9d3
Polina <Polina>
2016-11-15 21:24:10 +0200

    corrections on module5 solution

--- module5-solution/src/common/menu.service.js
+++ module5-solution/src/common/menu.service.js
@@ -15,6 +15,11 @@ function MenuService($http, ApiPath) {
     });
   };
 
+  service.getMenuItem = function (shortName) {
+    return $http.get(ApiPath + '/menu_items/' + shortName + '.json').then(function (response) {
+      return response.data;
+    });
+  };
 
   service.getMenuItems = function (category) {
     var config = {};
--- module5-solution/src/public/signup/signup.controller.js
+++ module5-solution/src/public/signup/signup.controller.js
@@ -11,20 +11,20 @@
 
     signUpCtrl.message = "";
 
-    signUpCtrl.submit = function() {
-      if(signUpCtrl.user.dish) {
-         MenuService.getMenuItems(signUpCtrl.user.dish).then(function(response) {
-           console.log('response', response);
-           if(response.menu_items.length) {
-             //save the user
-             SignupService.saveUser(signUpCtrl.user, response.category);
-             signUpCtrl.message = "Your information has been saved!";
-           }else {
-             signUpCtrl.message = "No such menu number exists";
-           }
+    var dish = {};
 
+    signUpCtrl.submit = function() {
+      signUpCtrl.message = "";
+      // if(signUpCtrl.user.dish) {
+         MenuService.getMenuItem(signUpCtrl.user.dish).then(function(response) {
+             //save the user
+             SignupService.saveUser(signUpCtrl.user, response);
+             signUpCtrl.message = "Your information has been saved!";
+           
+         }).catch(function(error) {
+           signUpCtrl.message = "No such menu number exists";
          });
-      }
+      // }
 
     };
 
