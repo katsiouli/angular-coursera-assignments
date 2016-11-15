bd5119eb8cb6c75fe4c25d2f7c05a585c71356e2
Polina <Polina>
2016-11-15 21:26:52 +0200

    change http to https

--- module5-solution/src/common/common.module.js
+++ module5-solution/src/common/common.module.js
@@ -2,7 +2,7 @@
 "use strict";
 
 angular.module('common', [])
-.constant('ApiPath', 'http://pkatsiouli.herokuapp.com')
+.constant('ApiPath', 'https://pkatsiouli.herokuapp.com')
 .config(config);
 
 config.$inject = ['$httpProvider'];
